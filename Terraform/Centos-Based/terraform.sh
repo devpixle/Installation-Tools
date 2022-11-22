e0633a437675ee8981b720a5ae39168b05de068a
root <root@PNode.i1jm3hjtgjvudcfvkppmc2cpxa.lx.internal.cloudapp.net>
2022-11-22 09:48:31 +0000

    Updated Packages and Tools

---
+++ Terraform/Centos-Based/terraform.sh
@@ -0,0 +1,9 @@
+#Terraform Installation on CentOS 7
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+
+sudo yum -y install terraform
+
+terraform -help
