e04fe46004f21ffcfc4ed52d6cc1e5df025680ff
root <root@ip-172-31-5-2.ap-south-1.compute.internal>
2022-12-08 12:01:02 +0000

    Some Basic Updates and Upgrades To Package

--- MongoDB/mongod.sh
+++ MongoDB/mongod.sh
@@ -16,7 +16,7 @@ cat << comment
   Once Instllation is done run the following command 
 comment 
 
-sudo wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 
 cat << comment 
   Create /etc/apt/sources.list.d/mongodb-org-5.0.list. I am adding this by using the touch command, you can create this file using your favorite editor.
@@ -24,11 +24,17 @@ comment
 
 sudo cd /etc/apt/sources.list.d/
 
-sudo touch mongodb-org-5.0.list
+sudo touch mongodb-org-6.0.list
 
 # Now add the following repo by the echo command.
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+# After Adding Repository Run These Commands as Mongodb Version 6 Does'nt Have This Package 
+
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+
+dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb 
 
 cat << comment 
  Finally, update the system after adding the repo and installing MongoDB.
